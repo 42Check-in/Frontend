6fa8414b7f1d7293c65d4e793a80551fc09409d2
jjongs2 <kijsong@student.42seoul.kr>
2023-08-16 13:09:37 +0900

    [style] 클래스 순서 정렬

--- components/common/Btn.tsx
+++ components/common/Btn.tsx
@@ -11,7 +11,7 @@ interface BtnProps {
 export default function Btn({ fontSize, onClick, px, py, text }: BtnProps): ReactElement {
   return (
     <button
-      className={`text-${fontSize} px-${px} py-${py} bg-[#6A70FF] hover:bg-[#6AA6FF] text-white font-bold rounded-[20px]`}
+      className={`text-${fontSize} px-${px} py-${py} rounded-[20px] bg-[#6A70FF] font-bold text-white hover:bg-[#6AA6FF]`}
       onClick={onClick}
     >
       {text}
