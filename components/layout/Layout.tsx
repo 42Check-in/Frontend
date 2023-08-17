875f4565ad54cb71722693e5011d73974f54f7b0
jjongs2 <kijsong@student.42seoul.kr>
2023-08-17 19:29:37 +0900

    [feat] 사이드바 조건부 렌더링
    메인 페이지에서는 사이드바 렌더링 X

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,16 +1,22 @@
+import { cls } from '@/styles/cls';
+import { useRouter } from 'next/router';
 import type { ReactElement } from 'react';
 
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps): ReactElement {
+  const { pathname } = useRouter();
+  const hasSidebar = pathname !== '/';
   return (
     <>
       <Header />
-      <div className='pt-20'>{children}</div>
+      {hasSidebar && <Sidebar />}
+      <div className={cls(hasSidebar ? 'ml-28' : '', 'pt-20')}>{children}</div>
     </>
   );
 }
