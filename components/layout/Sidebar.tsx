06b6e588615a2726eac84ec17045f987031806c2
jjongs2 <kijsong@student.42seoul.kr>
2023-08-17 20:57:58 +0900

    [feat] 사이드바 메뉴 클릭 시 페이지 이동

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,23 +1,30 @@
+import Link from 'next/link';
 import type { ReactElement } from 'react';
 
-import Btn from '../common/Btn';
-
 interface MenuProps {
+  href: string;
   text: string;
 }
 
-function Menu({ text }: MenuProps): ReactElement {
-  return <Btn fontSize='sm' onClick={() => {}} px='1' py='4' text={text} />;
+function Menu({ href, text }: MenuProps): ReactElement {
+  return (
+    <Link
+      href={href}
+      className='rounded-[20px] bg-[#6A70FF] px-1 py-3.5 text-center text-sm font-bold text-white hover:bg-[#6AA6FF]'
+    >
+      {text}
+    </Link>
+  );
 }
 
 export default function Sidebar(): ReactElement {
   return (
     <div className='fixed left-0 pt-20'>
       <div className='flex h-screen w-28 flex-col space-y-2.5 border-r border-[#909090] px-2 py-3.5'>
-        <Menu text='회의실 예약' />
-        <Menu text='외부인 방문' />
-        <Menu text='수요지식회' />
-        <Menu text='기자재 대여' />
+        <Menu href='/conference-rooms' text='회의실 예약' />
+        <Menu href='/visitors' text='외부인 방문' />
+        <Menu href='/presentations' text='수요지식회' />
+        <Menu href='/equipments' text='기자재 대여' />
       </div>
     </div>
   );
