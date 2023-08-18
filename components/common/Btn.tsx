16959400fd87fd9909183d189f78078cac646e01
oio337a <oio337a@gmail.com>
2023-08-18 23:09:57 +0900

    [feat] selectItem props 추가

--- components/common/Btn.tsx
+++ components/common/Btn.tsx
@@ -1,3 +1,4 @@
+import { cls } from '@/styles/cls';
 import type { MouseEventHandler, ReactElement } from 'react';
 
 interface BtnProps {
@@ -6,12 +7,13 @@ interface BtnProps {
   px: string;
   py: string;
   text: string;
+  selectItem?: string;
 }
 
-export default function Btn({ fontSize, onClick, px, py, text }: BtnProps): ReactElement {
+export default function Btn({ fontSize, onClick, px, py, text, selectItem }: BtnProps): ReactElement {
   return (
     <button
-      className={`rounded-[20px] bg-[#6A70FF] px-${px} py-${py} text-${fontSize} font-bold text-white hover:bg-[#6AA6FF]`}
+      className={cls(selectItem === text ? 'bg-[#6AA6FF]':'',`rounded-[20px] bg-[#6A70FF] px-${px} py-${py} text-${fontSize} font-bold text-white hover:bg-[#6AA6FF]`)}
       onClick={onClick}
     >
       {text}
