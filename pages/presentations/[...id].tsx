b27377ef822dc2b22f1f5cb8d5c5327577bf09bf
jjongs2 <kijsong@student.42seoul.kr>
2023-08-18 16:30:36 +0900

    [refactor] 확인 모달 컴포넌트 모듈화

--- pages/presentations/[...id].tsx
+++ pages/presentations/[...id].tsx
@@ -1,7 +1,5 @@
-import ModalButton from '@/components/common/ModalButton';
-import ModalText from '@/components/common/ModalText';
-import ModalWrapper from '@/components/common/ModalWrapper';
 import PresentationsForm from '@/components/form/PresentationsForm';
+import OkModal from '@/components/modal/OkModal';
 import { useState } from 'react';
 import type { ReactElement } from 'react';
 
@@ -10,15 +8,7 @@ export default function PresentationApplication(): ReactElement {
   return (
     <div>
       <PresentationsForm setShowModal={setShowModal} />
-      {showModal && (
-        <ModalWrapper>
-          <ModalText>
-            <p>신청이 완료되었습니다.</p>
-            <p>Vocal분의 승인을 기다려 주세요 :)</p>
-          </ModalText>
-          <ModalButton text='오예~~!!' />
-        </ModalWrapper>
-      )}
+      {showModal && <OkModal />}
     </div>
   );
 }
--- pages/visitors/[...id].tsx
+++ pages/visitors/[...id].tsx
@@ -1,7 +1,5 @@
-import ModalButton from '@/components/common/ModalButton';
-import ModalText from '@/components/common/ModalText';
-import ModalWrapper from '@/components/common/ModalWrapper';
 import VisitorsForm from '@/components/form/EquipmentsForm';
+import OkModal from '@/components/modal/OkModal';
 import { useState } from 'react';
 import type { ReactElement } from 'react';
 
@@ -10,15 +8,7 @@ export default function Equipments(): ReactElement {
   return (
     <>
       <VisitorsForm setShowModal={setShowModal} />
-      {showModal && (
-        <ModalWrapper>
-          <ModalText>
-            <p>신청이 완료되었습니다.</p>
-            <p>Vocal분의 승인을 기다려 주세요 :)</p>
-          </ModalText>
-          <ModalButton text='오예~~!!' />
-        </ModalWrapper>
-      )}
+      {showModal && <OkModal />}
     </>
   );
 }
