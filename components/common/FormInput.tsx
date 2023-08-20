083e54238b1a933e86cdf75d05e047efe188007d
oio337a <yongmipa@student.42seoul.kr>
2023-08-20 21:40:30 +0900

    [feat] useForm input에 적용

--- components/common/FormInput.tsx
+++ components/common/FormInput.tsx
@@ -1,4 +1,5 @@
 import type { ChangeEventHandler, ReactElement } from 'react';
+import { useFormContext } from 'react-hook-form';
 
 interface FormInputProps {
   title: string;
@@ -19,6 +20,10 @@ export default function FormInput({
   span = 'full',
   value,
 }: FormInputProps): ReactElement {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   return (
     <div className={`col-span-${span}`}>
       <label htmlFor={title} className='block text-sm font-medium leading-6 text-gray-900'>
@@ -27,13 +32,12 @@ export default function FormInput({
       <div className='mt-2'>
         <input
           type={type}
-          name={title}
           id={title}
           className='block w-full rounded-md border-0 px-3.5 py-2.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
           disabled={disabled}
-          onChange={onChange}
           placeholder={placeholder}
           value={value}
+          {...register(title, { required: true })}
         />
       </div>
     </div>
