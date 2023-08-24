53aed3bb3a4c5600385128700e395048f32d372f
jjongs2 <jjongs@hanyang.ac.kr>
2023-08-24 21:33:29 +0900

    [feat] 모달 버튼 컴포넌트 삭제

--- pages/equipments/index.tsx
+++ pages/equipments/index.tsx
@@ -1,5 +1,4 @@
 import Calendar from '@/components/calendar/Calendar';
-import ModalButton from '@/components/common/ModalButton';
 import ModalText from '@/components/common/ModalText';
 import ModalWrapper from '@/components/common/ModalWrapper';
 import { useState } from 'react';
@@ -13,8 +12,12 @@ export default function Equipments(): ReactElement {
         <ModalWrapper>
           <ModalText>신청 유형을 선택해 주세요.</ModalText>
           <div className='space-x-2'>
-            <ModalButton text='신규' />
-            <ModalButton text='연장' />
+            <button className='button-modal'>
+              <p>신규</p>
+            </button>
+            <button className='button-modal'>
+              <p>연장</p>
+            </button>
           </div>
         </ModalWrapper>
       )}
