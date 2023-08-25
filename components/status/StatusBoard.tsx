b45498d8b429f3f916bb4fd8031fd21edee57d37
oio337a <yongmipa@student.42seoul.kr>
2023-08-25 18:37:37 +0900

    [feat] status board 보칼과 카뎃 구분

--- components/status/StatusBoard.tsx
+++ components/status/StatusBoard.tsx
@@ -1,4 +1,5 @@
 import type FormInfo from '@/interfaces/FormInfo';
+import { cls } from '@/styles/cls';
 import apiController from '@/utils/apiController';
 import { type Dispatch, type ReactElement, type SetStateAction, useEffect, useState } from 'react';
 
@@ -25,10 +26,11 @@ const btnContent = [
 
 interface StatusBoardProps {
   setSelectFormInfo: Dispatch<SetStateAction<FormInfo>>;
+  vocal: boolean;
 }
 
-export default function StatusBoard({ setSelectFormInfo }: StatusBoardProps): ReactElement {
-  const [category, setCategory] = useState('equipments');
+export default function StatusBoard({ setSelectFormInfo, vocal }: StatusBoardProps): ReactElement {
+  const [category, setCategory] = useState('conference-rooms');
   const [responseDataList, setResponseDataList] = useState<FormInfo[]>([]);
 
   useEffect(() => {
@@ -41,16 +43,19 @@ export default function StatusBoard({ setSelectFormInfo }: StatusBoardProps): Re
     }
     void fecthForms();
   }, [category]);
+
   return (
     <div className='m-10 flex max-h-80 min-h-[80vh] min-w-max flex-col overflow-scroll rounded-xl border'>
       {/* 위에 버튼 4개있는 부분 */}
       <div className='sticky top-0 flex justify-between space-x-4 border-b-2 bg-white p-10 pb-4'>
         <div className='flex items-center space-x-2'>
-          <input
-            value='white'
-            type='checkbox'
-            className='mr-10 h-6 w-6 rounded border-gray-300 transition hover:ring-2 hover:ring-indigo-500 focus:ring-indigo-500'
-          />
+          {vocal && (
+            <input
+              value='white'
+              type='checkbox'
+              className='mr-10 h-6 w-6 rounded border-gray-300 transition hover:ring-2 hover:ring-indigo-500 focus:ring-indigo-500'
+            />
+          )}
           {btnContent.map((items) => (
             <div
               key={items.text}
@@ -58,19 +63,28 @@ export default function StatusBoard({ setSelectFormInfo }: StatusBoardProps): Re
                 setCategory(items.url);
               }}
             >
-              <Btn fontSize='lg' onClick={() => {}} px='8' py='2 ' text={`${items.text}`} />
+              <button
+                className={cls(
+                  category === items.url ? 'bg-[#6AA6FF]' : 'bg-[#6A70FF]',
+                  'rounded-[20px] px-5 py-2 font-bold text-white hover:bg-[#6AA6FF]',
+                )}
+              >
+                {items.text}
+              </button>
             </div>
           ))}
         </div>
-        <div className='flex space-x-4'>
-          <button className='rounded-full px-2 transition-colors hover:bg-[#6AA6FF] hover:text-white hover:shadow-xl'>
-            수락
-          </button>
-          <div className='my-2 border-2 border-gray-300' />
-          <button className='rounded-full px-2 transition-colors hover:bg-[#6AA6FF] hover:text-white hover:shadow-xl'>
-            거절
-          </button>
-        </div>
+        {vocal && (
+          <div className='flex space-x-4'>
+            <button className='rounded-full px-2 transition-colors hover:bg-[#6AA6FF] hover:text-white hover:shadow-xl'>
+              수락
+            </button>
+            <div className='my-2 border-2 border-gray-300' />
+            <button className='rounded-full px-2 transition-colors hover:bg-[#6AA6FF] hover:text-white hover:shadow-xl'>
+              거절
+            </button>
+          </div>
+        )}
       </div>
       <div className=' mt-6 space-y-5'>
         {responseDataList.map((item, i) => (
@@ -81,11 +95,13 @@ export default function StatusBoard({ setSelectFormInfo }: StatusBoardProps): Re
             }}
             className='mx-4 flex justify-between space-x-2 rounded-2xl border-2 px-6 py-8 text-xl shadow-xl transition duration-300 ease-in-out hover:bg-[#6AA6FF]'
           >
-            <input
-              value='white'
-              type='checkbox'
-              className='h-6 w-6 rounded border-gray-300 transition'
-            />
+            {vocal && (
+              <input
+                value='white'
+                type='checkbox'
+                className='h-6 w-6 rounded border-gray-300 transition'
+              />
+            )}
             <div className=''>{item.formInfo.date}</div>
             <div className=' border-2 border-gray-300' />
             <div className=''>13:00</div>
