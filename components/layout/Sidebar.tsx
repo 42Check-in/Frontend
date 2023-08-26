945c40155c20ae635090c0ec337eab5ef9702b3a
oio337a <oio337a@gmail.com>
2023-08-26 15:42:06 +0900

    [style] pt change

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -27,7 +27,7 @@ function Menu({ href, text }: MenuProps): ReactElement {
 
 export default function Sidebar(): ReactElement {
   return (
-    <div className='fixed left-0 z-10 bg-white pt-20'>
+    <div className='fixed left-0 z-10 bg-white pt-16'>
       <div className='flex h-screen w-28 flex-col space-y-2.5 border-r border-[#909090] px-2 py-3.5'>
         <Menu href='/conference-rooms' text='회의실 예약' />
         <Menu href='/visitors' text='외부인 초대' />
