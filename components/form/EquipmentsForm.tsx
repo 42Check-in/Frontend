fbde61e8fc0efdb0a9e58495bc5180776a0b876a
oio337a <oio337a@gmail.com>
2023-08-27 20:40:42 +0900

    [fix] disabled 버그 수정

--- components/form/EquipmentsForm.tsx
+++ components/form/EquipmentsForm.tsx
@@ -1,4 +1,5 @@
 import type EquipmentsFormInfo from '@/interfaces/EquipmentsFormInfo';
+import { useRouter } from 'next/router';
 import type { Dispatch, ReactElement, SetStateAction } from 'react';
 
 import FormAgreement from '../common/FormAgreement';
@@ -21,6 +22,9 @@ export default function EquipmentsForm({
   setShowModal,
   formInfo,
 }: EquipmentsFormProps): ReactElement {
+  const { pathname } = useRouter();
+  const myCheckin = pathname === '/my-checkin';
+
   return (
     <FormContainer>
       <div className='mx-auto max-w-2xl pb-5 text-gray-900'>
@@ -34,16 +38,16 @@ export default function EquipmentsForm({
             name='userName'
             title='신청자 이름'
             type='text'
-            value={formInfo.userName}
-            disabled={formInfo.userName !== null}
+            value={formInfo?.userName}
+            disabled={myCheckin}
             placeholder='실명을 알려 주세요. (예시: 정우성)'
           />
           <FormInput
             name='phoneNumber'
             title='연락처'
             type='text'
-            value={formInfo.phoneNumber}
-            disabled={formInfo.phoneNumber !== null}
+            value={formInfo?.phoneNumber}
+            disabled={myCheckin}
             placeholder='연락처를 입력해 주세요. (예시: 010-4242-4242)'
           />
           <FormSelect
@@ -51,8 +55,8 @@ export default function EquipmentsForm({
             title='대여 물품'
             options={EQUIPMENTS}
             span='1'
-            value={EQUIPMENTS[formInfo.equipment - 1]}
-            disabled={formInfo.equipment !== null}
+            value={EQUIPMENTS[formInfo?.equipment - 1]}
+            disabled={myCheckin}
             etcName='etcEquipment'
           />
           <FormSelect
@@ -60,36 +64,36 @@ export default function EquipmentsForm({
             title='대여 목적'
             options={PURPOSES}
             span='1'
-            value={PURPOSES[formInfo.purpose - 1]}
-            disabled={formInfo.purpose !== null}
+            value={PURPOSES[formInfo?.purpose - 1]}
+            disabled={myCheckin}
             etcName='etcPurpose'
           />
           <FormTextArea
             name='detail'
-            value={formInfo.detail}
-            disabled={formInfo.detail !== null}
+            value={formInfo?.detail}
+            disabled={myCheckin}
             title='활용 계획 (무엇을, 어떻게, 왜, 언제까지 4가지를 꼭 기재해 주세요.)'
             placeholder='상세히 기술해 주세요.'
           />
           <FormTextArea
             name='benefit'
             title='기대 효과'
-            value={formInfo.benefit}
-            disabled={formInfo.benefit !== null}
+            value={formInfo?.benefit}
+            disabled={myCheckin}
           />
           <FormSelect
             name='period'
             title='대여 기간'
-            value={PERIODS[formInfo.period]}
-            disabled={formInfo.period !== null}
+            value={PERIODS[formInfo?.period]}
+            disabled={myCheckin}
             options={PERIODS}
             span='1'
           />
           <FormInput
             name='returnDate'
             title='반납 예정일'
-            value={formInfo.returnDate}
-            disabled={formInfo.returnDate !== null}
+            value={formInfo?.returnDate}
+            disabled={myCheckin}
             type='date'
             span='1'
           />
