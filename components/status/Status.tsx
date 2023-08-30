243515ea3d101f4c4e635c882bf796265aee37f8
oio337a <oio337a@gmail.com>
2023-08-30 22:14:56 +0900

    [fix] 상태 enum 값

--- components/status/Status.tsx
+++ components/status/Status.tsx
@@ -1,6 +1,5 @@
 import type FormInfo from '@/interfaces/FormInfo';
 import { cls } from '@/styles/cls';
-import useHandleMouseIndex from '@/utils/handleMouse';
 import { type ReactElement } from 'react';
 
 interface StatusProps {
@@ -11,7 +10,15 @@ interface StatusProps {
   vocal?: boolean;
 }
 
-const STATUS = ['승인 대기', '승인', '스케줄 등록 완료',  '아젠다 등록', '강의 완료', '차례 대기 중'];
+const STATUS = [
+  '신청 중',
+  '승인',
+  '스케줄 등록 완료',
+  '아젠다 등록',
+  '강의 완료',
+  '차례 대기 중',
+];
+
 export default function Status({
   status,
   setShowModal,
