b6a9ad83bac8a3bbe476daf05c421a373d203c66
oio337a <yongmipa@student.42seoul.kr>
2023-09-04 13:46:58 +0900

    [feat] 승인/미승인 드롭다운 버튼

--- components/status/VocalStatusBoard.tsx
+++ components/status/VocalStatusBoard.tsx
@@ -1,6 +1,7 @@
 import type { ApplicationFormInfo } from '@/interfaces/FormInfo';
 import { cls } from '@/styles/cls';
 import apiController from '@/utils/apiController';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type { ParsedUrlQueryInput } from 'querystring';
 import { useEffect, useState } from 'react';
@@ -29,6 +30,7 @@ export default function StatusBoard({
   const [checked, setChecked] = useState(false);
   const [formInfos, setFormInfos] = useState<ApplicationFormInfo[]>();
   const [selectedFormInfo, setSelectedFormInfo] = useState<ApplicationFormInfo>();
+  const [showDropDown, setShowDropDown] = useState(false);
 
   useEffect(() => {
     if (category === undefined) return;
@@ -77,8 +79,8 @@ export default function StatusBoard({
 
   return (
     <div className='z-10 m-4 flex h-full max-h-[79vh] min-w-[405px] flex-col overflow-auto rounded-xl border bg-white dark:bg-slate-800 lg:w-[800px]'>
-      <div className='sticky top-0 flex justify-between space-x-4 border-b-2 bg-white p-4 dark:bg-slate-700'>
-        <div className='flex w-full items-center justify-between space-x-2'>
+      <div className='sticky top-0 flex items-center justify-between space-x-4 border-b-2 bg-white p-4 dark:bg-slate-700'>
+        <div className='flex w-full items-center space-x-2'>
           <input
             value='white'
             type='checkbox'
@@ -94,6 +96,59 @@ export default function StatusBoard({
             )}
           />
           <div className='flex space-x-3'>{btnBox}</div>
+        </div>
+        <div className='relative inline-block text-left'>
+          <div>
+            <button
+              type='button'
+              className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+              id='menu-button'
+              aria-expanded='true'
+              aria-haspopup='true'
+              onClick={() => setShowDropDown(!showDropDown)}
+            >
+              Options
+              <svg
+                className='-mr-1 h-5 w-5 text-gray-400'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  fill-rule='evenodd'
+                  d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
+                  clip-rule='evenodd'
+                />
+              </svg>
+            </button>
+          </div>
+          {showDropDown && (
+            <div
+              className='absolute right-0 z-10 mt-2 w-max origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+              role='menu'
+              aria-orientation='vertical'
+              aria-labelledby='menu-button'
+            >
+              <div className='py-1' role='none'>
+                <Link
+                  href='#'
+                  className='block px-4 py-2 text-sm text-gray-700'
+                  role='menuitem'
+                  id='menu-item-0'
+                >
+                  미승인 항목
+                </Link>
+                <Link
+                  href='#'
+                  className='block px-4 py-2 text-sm text-gray-700'
+                  role='menuitem'
+                  id='menu-item-1'
+                >
+                  승인된 항목
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className='mt-6 w-full'>
